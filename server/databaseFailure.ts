21596e333464ac57584082288d83ccf5f8961ac5
Simon Mika <simon@mika.se>
2019-03-22 12:47:10 +0100

    Added details.

--- server/databaseFailure.ts
+++ server/databaseFailure.ts
@@ -1,11 +1,11 @@
 import { Error } from "../Error"
 import { Response } from "../Response"
 
-export function databaseFailure(): Error & { response: Response } {
-	return Response.add({ status: 502, type: "database failure" })
+export function databaseFailure(details?: any): Error & { details?: any, response: Response } {
+	return Response.add({ status: 502, type: "database failure", details })
 }
 export namespace databaseFailure {
-	export function is(value: any): value is { status: 502, type: "database failure" } {
+	export function is(value: any): value is { status: 502, type: "database failure", details?: any } {
 		return Error.is(value) && value.status == 502 && value.type == "database failure"
 	}
 }
