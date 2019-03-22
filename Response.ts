f0560c6dd25354dde041f5b0d3dfe996d68eb583
Johnny Olsson <23193083+johnnysolsson@users.noreply.github.com>
2019-03-22 17:11:24 +0100

    Name of type: data > body. More logics in Result (#2)

--- Response.ts
+++ Response.ts
@@ -3,7 +3,8 @@ import { Result } from "./Result"
 export class Response {
 	readonly headers: { [header: string]: string | string[] }
 	private constructor(readonly status: number, readonly body?: any) {
-		this.headers = body ? { "content-type": "application/json; charset=utf-8" } : {}
+		this.headers = typeof(body) == "string" ? { "content-type": "text/html; charset=utf-8" } : 
+			body != undefined && body != null ? { "content-type": "application/json; charset=utf-8" } : {}
 	}
 	static add<T extends Result>(result: T): T & { response: Response } {
 		return { ...result, response: new Response(result.status, result.status >= 400 ? result : Result.getBody(result)) }
--- success/ok.ts
+++ success/ok.ts
@@ -1,8 +1,8 @@
 import { Result } from "../Result"
 import { Response } from "../Response"
 
-export function ok<T>(data: T): Result & { data: T, response: Response } {
-	return Response.add({ status: 200, data })
+export function ok<T>(body: T): Result & { body: T, response: Response } {
+	return Response.add({ status: 200, body })
 }
 export namespace ok {
 	export function is(value: any): value is { status: 200, data: any } {
