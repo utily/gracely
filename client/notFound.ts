74aec29bab8a2ae97a3a0eb6f80de79680e3e869
Simon Mika <simon@mika.se>
2019-03-22 17:49:03 +0100

    Removed misstakes.

--- client/notFound.ts
+++ client/notFound.ts
@@ -1,11 +1,11 @@
 import { Error } from "../Error"
 import { Response } from "../Response"
 
-export function notFound(name: string, type: string, description: string): Error & { argument: { name: string, type: string, description: string }, response: Response } {
-	return Response.add({ status: 404, type: "not found", argument: { name, type, description } })
+export function notFound(): Error & { response: Response } {
+	return Response.add({ status: 404, type: "not found" })
 }
 export namespace notFound {
-	export function is(value: any): value is { status: 404, type: "not found", argument: { name: string, type: string, description: string } } {
+	export function is(value: any): value is { status: 404, type: "not found" } {
 		return Error.is(value) && value.status == 404 && value.type == "not found"
 	}
 }
