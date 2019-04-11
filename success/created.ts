036f6a567209f2d6cbfa36fd288a10050b2d6314
Simon Mika <simon@mika.se>
2019-04-11 18:06:31 +0200

    Fixed bug.

--- success/created.ts
+++ success/created.ts
@@ -1,11 +1,14 @@
 import { Result } from "../Result"
 import { Response } from "../Response"
 
-export function created<T>(data: T): Result & { data: T, response: Response } {
-	return Response.add({ status: 201, data })
+export function created<T>(body: T): Result & { body: T, response: Response } {
+	return Response.add({ status: 201, body })
 }
 export namespace created {
-	export function is(value: any): value is { status: 201, data: any } {
-		return Result.is(value) && value.status == 201
+	export function is(value: any): value is { status: 201, body: any } {
+		return typeof(value) == "object" &&
+			value.body &&
+			Result.is(value) &&
+			value.status == 201
 	}
 }
--- success/ok.ts
+++ success/ok.ts
@@ -5,7 +5,10 @@ export function ok<T>(body: T): Result & { body: T, response: Response } {
 	return Response.add({ status: 200, body })
 }
 export namespace ok {
-	export function is(value: any): value is { status: 200, data: any } {
-		return Result.is(value) && value.status == 200
+	export function is(value: any): value is { status: 200, body: any } {
+		return typeof(value) == "object" &&
+			value.body &&
+			Result.is(value) &&
+			value.status == 200
 	}
 }
