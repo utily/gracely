205dd3a9cdef91f3b516e8c33362b2ffccca93e2
Simon Mika <simon@mika.se>
2019-05-23 13:59:20 +0200

    Fixed response code.

--- client/unauthorized.ts
+++ client/unauthorized.ts
@@ -2,10 +2,10 @@ import { Error } from "../Error"
 import { Response } from "../Response"
 
 export function unauthorized(): Error & { response: Response } {
-	return Response.add({ status: 404, type: "not authorized" })
+	return Response.add({ status: 401, type: "not authorized" })
 }
 export namespace unauthorized {
-	export function is(value: any): value is { status: 404, type: "not authorized" } {
-		return Error.is(value) && value.status == 404 && value.type == "not authorized"
+	export function is(value: any): value is { status: 401, type: "not authorized" } {
+		return Error.is(value) && value.status == 401 && value.type == "not authorized"
 	}
 }
