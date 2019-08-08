3567ed1acd50a7d462a8637197ddcf8da51c2516
Simon Mika <simonmika@users.noreply.github.com>
2019-08-08 19:37:08 +0200

    Content type (#13)
    * Improved content type assignment.
    
    * Updated dependencies.

--- Response.ts
+++ Response.ts
@@ -3,8 +3,11 @@ import { Result } from "./Result"
 export class Response {
 	readonly headers: { [header: string]: string | string[] }
 	private constructor(readonly status: number, readonly body?: any) {
-		this.headers = typeof(body) == "string" ? { "content-type": "text/html; charset=utf-8" } :
-			body != undefined && body != null ? { "content-type": "application/json; charset=utf-8" } : {}
+		this.headers = typeof(body) == "string" ? (
+			body.startsWith("<!doctype") ? { "content-type": "text/html; charset=utf-8" } : 
+			/^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/.test(body) ? { "content-type": "application/jwt; charset=utf-8" } :
+			{ "content-type": "text/plain; charset=utf-8" }
+		) : body != undefined && body != null ? { "content-type": "application/json; charset=utf-8" } : {}
 	}
 	static has(value: any): value is { response: Response } {
 		return typeof(value) == "object" && value.response instanceof Response
