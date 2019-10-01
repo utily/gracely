c40f1db5b6e122f0dbb02e6575ea246c194de0bd
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2019-10-01 10:25:49 +0200

    Added found and worked on redirect

--- Response.ts
+++ Response.ts
@@ -1,13 +1,18 @@
 import { Result } from "./Result"
+import * as redirect from "./redirect"
 
 export class Response {
 	readonly headers: { [header: string]: string | string[] }
 	private constructor(readonly status: number, readonly body?: any) {
-		this.headers = typeof(body) == "string" ? (
-			body.startsWith("<!doctype") ? { "content-type": "text/html; charset=utf-8" } : 
+		this.headers =
+		(redirect.permanent.is(body) || redirect.found.is(body)) ? { location: body.location } :
+		typeof(body) == "string" ? (
+			body.startsWith("<!doctype") ? { "content-type": "text/html; charset=utf-8" } :
 			/^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/.test(body) ? { "content-type": "application/jwt; charset=utf-8" } :
 			{ "content-type": "text/plain; charset=utf-8" }
-		) : body != undefined && body != null ? { "content-type": "application/json; charset=utf-8" } : {}
+		) :
+		body != undefined && body != null ? { "content-type": "application/json; charset=utf-8" } :
+		{}
 	}
 	static has(value: any): value is { response: Response } {
 		return typeof(value) == "object" && value.response instanceof Response
