8a06320baa7eb2f173f1401f6cc35f24a0cb0cec
Simon Mika <simonmika@users.noreply.github.com>
2020-03-23 14:51:21 +0100

    Added details and reference properties. (#25)

--- client/malformedContent.ts
+++ client/malformedContent.ts
@@ -1,11 +1,11 @@
 import { Error } from "../Error"
 import { Response } from "../Response"
 
-export function malformedContent(property: string, type: string, description: string): Error & { content: { property: string, type: string, description: string }, response: Response } {
+export function malformedContent(property: string, type: string, description: string, details?: any): Error & { content: { property: string, type: string, description: string, details?: any }, response: Response } {
 	return Response.add({ status: 400, type: "malformed content", content: { property, type, description } })
 }
-export namespace invalidContent {
-	export function is(value: any): value is { status: 400, type: "malformed content", content: { property: string, type: string, description: string } } {
+export namespace malformedContent {
+	export function is(value: any): value is { status: 400, type: "malformed content", content: { property: string, type: string, description: string, details?: any } } {
 		return Error.is(value) && value.status == 400 && value.type == "malformed content"
 	}
 }
