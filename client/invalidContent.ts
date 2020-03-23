5234f0b83c51ddb448872731f7b67a7ded722fe9
Simon Mika <simon@mika.se>
2020-03-23 14:36:23 +0100

    Added details and reference properties.

--- client/invalidContent.ts
+++ client/invalidContent.ts
@@ -1,11 +1,11 @@
 import { Error } from "../Error"
 import { Response } from "../Response"
 
-export function invalidContent(type: string, description: string): Error & { content: { type: string, description: string }, response: Response } {
+export function invalidContent(type: string, description: string, details?: any): Error & { content: { type: string, description: string, details?: any }, response: Response } {
 	return Response.add({ status: 400, type: "invalid content", content: { type, description } })
 }
 export namespace invalidContent {
-	export function is(value: any): value is { status: 400, type: "invalid content", content: { type: string, description: string } } {
+	export function is(value: any): value is { status: 400, type: "invalid content", content: { type: string, description: string, details?: any } } {
 		return Error.is(value) && value.status == 400 && value.type == "invalid content"
 	}
 }
--- server/backendFailure.ts
+++ server/backendFailure.ts
@@ -1,11 +1,17 @@
 import { Error } from "../Error"
 import { Response } from "../Response"
 
-export function backendFailure(details?: any): Error & { details?: any, response: Response } {
-	return Response.add({ status: 502, type: "backend failure", details })
+export function backendFailure(details?: any): Error & { backend: string, details?: any, reference?: any, response: Response }
+export function backendFailure(backend: string, details?: any, reference?: any): Error & { backend: string, details?: any, reference?: any, response: Response }
+export function backendFailure(backend: string | any, details?: any, reference?: any): Error & { backend: string, details?: any, reference?: any, response: Response } {
+	if (backend != "string" && details == undefined && reference == undefined) {
+		details = backend
+		backend = "unknown"
+	}
+	return Response.add({ status: 502, type: "backend failure", backend, details, reference })
 }
 export namespace backendFailure {
-	export function is(value: any): value is { status: 502, type: "backend failure", details?: any } {
-		return Error.is(value) && value.status == 502 && value.type == "backend failure"
+	export function is(value: any): value is { status: 502, type: "backend failure", backend: string, details?: any, reference?: any } {
+		return Error.is(value) && value.status == 502 && value.type == "backend failure" && typeof (value as any).backend == "string"
 	}
 }
