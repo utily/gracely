89eb5a2731ef416ac2fdab4b097fb079993d8eb8
Robin Nedergård <robin.nedergard@payfunc.com>
2020-08-24 15:53:15 +0200

    adding details to malformed content

--- client/malformedContent.ts
+++ client/malformedContent.ts
@@ -6,7 +6,7 @@ export function malformedContent(
 	description: string,
 	details?: any
 ): Error & { content: { property: string; type: string; description: string; details?: any } } {
-	return { status: 400, type: "malformed content", content: { property, type, description } }
+	return { status: 400, type: "malformed content", content: { property, type, description, details } }
 }
 export namespace malformedContent {
 	export function is(
