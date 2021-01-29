1d4dc1636ff26571f0740d4c0c145710bae9b8e4
fredrikbryntesson <fredrikbryntesson@users.noreply.github.com>
2021-01-29 09:46:23 +0100

    Fixed spelling of misconfigured (#45)
    Co-authored-by: fredrikbryntesson <fredrik.bryntesson@payfunc.com>

--- server/misconfigured.ts
+++ server/misconfigured.ts
@@ -1,9 +1,9 @@
 import { Error } from "../Error"
 
-export function missconfigured(key: string, description: string): Error & { key: string; description: string } {
+export function misconfigured(key: string, description: string): Error & { key: string; description: string } {
 	return { status: 503, type: "missing configuration", key, description }
 }
-export namespace missconfigured {
+export namespace misconfigured {
 	export function is(value: any): value is { status: 503; type: "missing configuration"; details?: any } {
 		return Error.is(value) && value.status == 503 && value.type == "missing configuration"
 	}
