b064ad51cb74bff0a81d2e808a43448b2e60e2b4
Robin Nedergård <robin.nedergard@payfunc.com>
2021-11-10 17:58:07 +0100

    Added unauthorized response options. (#56)
    * Added unauthorized response options.
    
    * Fixed character case.

--- Result.ts
+++ Result.ts
@@ -10,13 +10,20 @@ export namespace Result {
 	export function is(value: any | Result): value is Result {
 		return (
 			typeof value == "object" &&
+			value &&
 			typeof value.status == "number" &&
 			(value.header == undefined ||
 				(typeof value.header == "object" && (value.header.eTag == undefined || typeof value.header.eTag == "string")))
 		)
 	}
 	export function hasBody(value: any | Result): value is Result & { body: any } {
-		return typeof value == "object" && typeof value.status == "number" && value.body != undefined && value.body != null
+		return (
+			typeof value == "object" &&
+			value &&
+			typeof value.status == "number" &&
+			value.body != undefined &&
+			value.body != null
+		)
 	}
 	export function getBody(value: any | Result, fallback?: any): any {
 		return hasBody(value) ? value.body : fallback
