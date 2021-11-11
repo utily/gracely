bad81ea0516ad8b48c2267b614c252ea06316ba2
Robin Nedergård <60611967+robinnedergard@users.noreply.github.com>
2021-11-11 09:49:31 +0100

    Added unauthorized response options. (#56) (#58)
    * Added unauthorized response options.
    
    * Fixed character case.

--- client/Unauthorized.ts
+++ client/Unauthorized.ts
@@ -10,30 +10,36 @@ export interface Unauthorized extends Error {
 
 export function unauthorized(): Error
 export function unauthorized(error: string): Error
-export function unauthorized(scheme: "basic",  parameter?: { realm?: string, charset?: "UTF-8" }): Error
-export function unauthorized(scheme?: "basic" | string, parameter?: { realm?: string, charset?: "UTF-8" }): Error {
+export function unauthorized(scheme: "basic", parameter?: { realm?: string; charset?: "UTF-8" }): Error
+export function unauthorized(scheme?: "basic" | string, parameter?: { realm?: string; charset?: "UTF-8" }): Error {
 	const result: Error = { status: 401, type: "not authorized" }
 	if (scheme == "basic")
-		result.header = { wwwAuthenticate: `${scheme[0].toUpperCase()}${scheme.slice(1)} ${Object.entries(parameter ?? {}).map(([p, t]) => `${p}=${t}`).join(", ")}` }
+		result.header = {
+			wwwAuthenticate: `${scheme[0].toUpperCase()}${scheme.slice(1)}${
+				typeof parameter == "object" && parameter && Object.keys(parameter).length > 0
+					? ` ${Object.entries(parameter ?? {})
+							.map(([p, t]) => `${p}=${t}`)
+							.join(", ")}`
+					: ""
+			}`,
+		}
 	else
 		result.error = scheme
 	return result
 }
-export namespace unauthorized {
-	export function is(value: any): value is { status: 401; type: "not authorized" } {
-		return Error.is(value) && value.status == 401 && value.type == "not authorized"
-	}
-}
-
 
 export namespace Unauthorized {
 	export function is(value: any): value is Unauthorized {
 		return (
 			typeof value == "object" &&
+			value &&
 			value.status == 401 &&
 			value.type == "not authorized" &&
 			(value.error == undefined || typeof value.error == "string") &&
-			(value.header == undefined || typeof value.header == "object" && (value.header.wwwAuthenticate == undefined || typeof value.header.wwwAuthenticate == "string")) &&
+			(value.header == undefined ||
+				(typeof value.header == "object" &&
+					value.header &&
+					(value.header.wwwAuthenticate == undefined || typeof value.header.wwwAuthenticate == "string"))) &&
 			Result.is(value)
 		)
 	}
