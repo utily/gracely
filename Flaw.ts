b4f6203a3538d538517c9151ede3c4b480d673bf
Lasse Löfquist <github@tvartom.com>
2023-03-01 15:05:26 +0100

    Flaw.property can be number (for arrays).

--- Flaw.ts
+++ Flaw.ts
@@ -1,5 +1,5 @@
 export interface Flaw {
-	property?: string
+	property?: string | number
 	type: string
 	flaws?: Flaw[]
 	condition?: string
@@ -9,7 +9,7 @@ export namespace Flaw {
 	export function is(value: any | Flaw): value is Flaw {
 		return (
 			typeof value == "object" &&
-			(value.property == undefined || typeof value.property == "string") &&
+			(value.property == undefined || typeof value.property == "string" || typeof value.property == "number") &&
 			typeof value.type == "string" &&
 			(value.flaws == undefined || (Array.isArray(value.flaws) && value.flaws.every(Flaw.is))) &&
 			(value.condition == undefined || typeof value.condition == "string")
