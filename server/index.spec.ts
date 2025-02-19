2e65faf6cfb4c50ad71d0b1f57fddd86d66779d1
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2025-02-19 16:55:40 +0100

    Updated to the new configs. (#75)
    * Updated to the new configs.
    
    * Removed Renamer from spelling list.
    
    * Switched to v8 coverage.
    
    * Fixed vulnerabilities.
    
    * Fixed package.

--- server/index.spec.ts
+++ server/index.spec.ts
@@ -1,4 +1,3 @@
-import { describe, expect, it } from "vitest"
 import * as gracely from "../index"
 
 describe("server", () => {
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite"
+
+export default defineConfig({
+	test: {
+		typecheck: { tsconfig: "./tsconfig.json" },
+		coverage: {
+			reporter: ["text", "json", "html"],
+			enabled: true,
+			all: true,
+			cleanOnRerun: true,
+			thresholds: { statements: 80, branches: 90, functions: 65, lines: 80 },
+		},
+		globals: true,
+		include: ["**/*.spec.[tj]s"],
+		testTimeout: 20000,
+		isolate: false,
+		exclude: ["node_modules", "dist"],
+		server: { deps: { inline: [] } },
+	},
+})
