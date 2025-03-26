4d1b53d54bca48393dfb6dccea87b3b8dce50f51
marcushanses <79835961+marcushanses@users.noreply.github.com>
2025-03-26 13:28:44 +0000

    Allow content in flaw to be false. (#76)
    * Allow content in flaw to be false.
    
    * Test and audit fixes.

--- client/FlawedContent.ts
+++ client/FlawedContent.ts
@@ -5,11 +5,11 @@ import { Result } from "../Result"
 export interface FlawedContent extends Error {
 	status: 400
 	type: "flawed content"
-	content: Flaw
+	content: Flaw | false
 	error?: string
 }
 
-export function flawedContent(content: Flaw, error?: string): FlawedContent {
+export function flawedContent(content: Flaw | false, error?: string): FlawedContent {
 	return { status: 400, type: "flawed content", content, error }
 }
 
@@ -19,7 +19,7 @@ export namespace FlawedContent {
 			typeof value == "object" &&
 			value.status == 400 &&
 			value.type == "flawed content" &&
-			Flaw.is(value.content) &&
+			(Flaw.is(value.content) || value.content === false) &&
 			(value.error == undefined || typeof value.error == "string") &&
 			Result.is(value)
 		)
